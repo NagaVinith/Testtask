202271a4b583e54afdc488470a84f7b7fdb53689
Lab-1 <Lab-1@DESKTOP-UHM8TAB>
2019-12-03 14:41:34 +0530

    this is commit message

---
+++ MyProject/src/GetSetDemo.java
@@ -0,0 +1,18 @@
+
+public class GetSetDemo {
+	String MyBankName;
+	int MybankAccountNumber;
+	public String getMyBankName() {
+		return MyBankName;
+	}
+	public void setMyBankName(String myBankName) {
+		MyBankName = myBankName;
+	}
+	public int getMybankAccountNumber() {
+		return MybankAccountNumber;
+	}
+	public void setMybankAccountNumber(int mybankAccountNumber) {
+		MybankAccountNumber = mybankAccountNumber;
+	}
+
+}
